e0894aa25ea38d20b8c73cb683f47931f79d146e
Anabel Ramirez and Buna Ezeofor <buna@>
2017-04-25 13:05:20 -0700

    simplifies javascript for toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
-  $("li#JavaScript").click(function() {
-  $("p").removeClass();
-  $("p#jsdef").toggle();
+  $('li').click(function(){
+       $(this).next('p').toggle();
   });
-  $("li#Operators").click(function() {
-  $("p").removeClass();
-  $("p#opdef").toggle();
-  });
-  $("li#Variables").click(function() {
-  $("p").removeClass();
-  $("p#vardef").toggle();
-  });
-  $("li#Conventions").click(function() {
-  $("p").removeClass();
-  $("p#condef").toggle();
-  $("li").children("p").hide()
-  });
+
 });
